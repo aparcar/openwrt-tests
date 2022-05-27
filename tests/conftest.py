e8f5d816adcb94e3232c789a4b02fcd9622e813c
Paul Spooren <mail@aparcar.org>
2022-05-27 22:35:50 +0200

    allow to add --target
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,22 @@ import pytest
 import pexpect
 import sys
 
+def pytest_addoption(parser):
+    parser.addoption("--target", action="store", default="x86/64")
 
 class Qemu:
-    def __init__(self):
+    def __init__(self, pytestconfig):
         self.child = None
+        self.pytestconfig = pytestconfig
 
     def start(self):
         target = "x86"
         subtarget = "64"
+        target, subtarget = self.pytestconfig.getoption('target').split("/")
         qemu_cmd = (
             f"./scripts/qemustart {target} {subtarget} --netdev user,id=wan "
             "-netdev user,id=lan,net=192.168.1.0/24,dhcpstart=192.168.1.100,restrict=yes,hostfwd=tcp::8022-:22 "
-            "-net nic,netdev=wan -net nic,netdev=lan"
+            "-net nic,netdev=wan -net nic,netdev=lan -device virtio-rng-pci"
         )
         self.child = pexpect.spawn(qemu_cmd, logfile=sys.stdout.buffer)
         self.child.expect("Please press Enter to activate this console.", timeout=80)
@@ -44,8 +48,8 @@ class Qemu:
 
 
 @pytest.fixture
-def dut():
-    dut = Qemu()
+def dut(pytestconfig):
+    dut = Qemu(pytestconfig)
     dut.start()
     yield dut
     dut.stop()
