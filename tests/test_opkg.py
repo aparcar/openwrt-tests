060b291fae1b77b709b1ed9593c8aba8f1354730
Hauke Mehrtens <hauke@hauke-m.de>
2022-05-29 12:39:40 +0200

    fix opkg test

--- tests/test_opkg.py
+++ tests/test_opkg.py
@@ -5,19 +5,12 @@ def test_opkg(dut):
     # Todo try to find a better way to detect that the WAN is working.
     time.sleep(5)
 
-    dut.send_cmd("opkg update")
-    dut.expect("https://downloads.openwrt.org/")
-    dut.expect("Signature check passed.")
+    dut.send_cmd("opkg update", "Signature check passed.")
 
-    dut.send_cmd("opkg list-installed")
-    dut.expect("procd ")
+    dut.send_cmd("opkg list-installed", "procd ")
 
-    dut.send_cmd("opkg install iperf3")
-    dut.expect("Installing iperf3")
-    dut.expect("Configuring iperf3")
+    dut.send_cmd("opkg install iperf3", "Configuring iperf3")
 
-    dut.send_cmd("opkg list-installed")
-    dut.expect("iperf3 ")
+    dut.send_cmd("opkg list-installed", "iperf3 ")
 
-    dut.send_cmd("iperf3 -v")
-    dut.expect("cJSON")
+    dut.send_cmd("iperf3 -v", "cJSON")
