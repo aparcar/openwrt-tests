5651135a51f4567372ab326a103ff2f3f7b51da6
Paul Spooren <mail@aparcar.org>
2024-05-06 23:35:07 +0200

    tests: use SSH for most tests
    When using real devices, this should add a better reliability.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_opkg.py
+++ tests/test_opkg.py
@@ -1,8 +1,8 @@
-def test_opkg_procd_installed(shell_command):
-    assert "procd" in "\n".join(shell_command.run("opkg list-installed")[0])
+def test_opkg_procd_installed(ssh_command):
+    assert "procd" in "\n".join(ssh_command.run("opkg list-installed")[0])
 
 
-def test_opkg_install_tmate(shell_command):
-    shell_command.run("opkg update")
-    shell_command.run("opkg install tmate")
-    assert "tmate" in "\n".join(shell_command.run("opkg list-installed")[0])
+def test_opkg_install_tmate(ssh_command):
+    ssh_command.run("opkg update")
+    ssh_command.run("opkg install tmate")
+    assert "tmate" in "\n".join(ssh_command.run("opkg list-installed")[0])
