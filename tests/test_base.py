5117e07ffeb6ba5b7234424f55416b637066bc85
Paul Spooren <mail@aparcar.org>
2024-05-23 10:53:10 +0200

    tests: add kernel error test
    Checks for errors during boot inspired by Petr work[1]
    
    [1]: https://gitlab.com/prpl-foundation/prplos/prplos/-/blob/prplos/.gitlab/tests/cram/generic/check-for-crashes.t
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,5 @@
 from conftest import ubus_call
+import re
 
 
 def test_shell(shell_command):
@@ -21,3 +22,27 @@ def test_ubus_system_board(shell_command):
 
 def test_ssh(ssh_command):
     ssh_command.run("true")
+
+
+def test_kernel_errors(shell_command):
+    logread = "\n".join(shell_command.run("logread")[0])
+    print(logread)
+    assert (
+        re.search(
+            r"(traps:.*general protection|segfault at [[:digit:]]+ ip.*error.*in)",
+            logread,
+        )
+        is None
+    )
+    assert re.search(r"do_page_fault\(\): sending", logread) is None
+    assert re.search(r"Unable to handle kernel.*address", logread) is None
+    assert re.search(r"(PC is at |pc : )([^+\[ ]+).*", logread) is None
+    assert re.search(r"epc\s+:\s+\S+\s+([^+ ]+).*", logread) is None
+    assert re.search(r"EIP: \[<.*>\] ([^+ ]+).*", logread) is None
+    assert (
+        re.search(
+            r"RIP: [[:xdigit:]]{4}:(\[<[[:xdigit:]]+>\] \[<[[:xdigit:]]+>\] )?([^+ ]+)\+0x.*",
+            logread,
+        )
+        is None
+    )
