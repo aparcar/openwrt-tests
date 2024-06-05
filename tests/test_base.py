093630999ec53f6a6880fa02e2576de70a3c737b
Paul Spooren <mail@aparcar.org>
2024-06-05 17:19:32 +0200

    tests: refactor kernel_errors test
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_base.py
+++ tests/test_base.py
@@ -26,23 +26,17 @@ def test_ssh(ssh_command):
 
 def test_kernel_errors(shell_command):
     logread = "\n".join(shell_command.run("logread")[0])
-    print(logread)
-    assert (
-        re.search(
-            r"(traps:.*general protection|segfault at [[:digit:]]+ ip.*error.*in)",
-            logread,
-        )
-        is None
-    )
-    assert re.search(r"do_page_fault\(\): sending", logread) is None
-    assert re.search(r"Unable to handle kernel.*address", logread) is None
-    assert re.search(r"(PC is at |pc : )([^+\[ ]+).*", logread) is None
-    assert re.search(r"epc\s+:\s+\S+\s+([^+ ]+).*", logread) is None
-    assert re.search(r"EIP: \[<.*>\] ([^+ ]+).*", logread) is None
-    assert (
-        re.search(
-            r"RIP: [[:xdigit:]]{4}:(\[<[[:xdigit:]]+>\] \[<[[:xdigit:]]+>\] )?([^+ ]+)\+0x.*",
-            logread,
-        )
-        is None
-    )
+
+    error_patterns = [
+        r"traps:.*general protection",
+        r"segfault at [[:digit:]]+ ip",
+        r"error.*in",
+        r"do_page_fault\(\): sending",
+        r"Unable to handle kernel.*address",
+        r"(PC is at |pc : )([^+\[ ]+).*",
+        r"epc\s+:\s+\S+\s+([^+ ]+).*",
+        r"EIP: \[<.*>\] ([^+ ]+).*",
+    ]
+
+    for pattern in error_patterns:
+        assert re.search(pattern, logread) is None, f"Found kernel error: {pattern}"
