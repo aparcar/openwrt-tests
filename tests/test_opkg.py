2a9fcd93747d670623f2b3f15ab749a638dedd0e
Paul Spooren <mail@aparcar.org>
2024-06-05 16:52:15 +0200

    tests: use correct marks for opkg and install ucert
    ucert is part of openwrt.git, use that instead of tmate
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_opkg.py
+++ tests/test_opkg.py
@@ -1,11 +1,12 @@
 import pytest
 
 
+@pytest.mark.lg_feature("opkg")
 def test_opkg_procd_installed(ssh_command):
     assert "procd" in "\n".join(ssh_command.run("opkg list-installed")[0])
 
-@pytest.mark.lg_feature("online")
-def test_opkg_install_tmate(ssh_command):
+@pytest.mark.lg_feature(["online", "opkg"])
+def test_opkg_install_ucert(ssh_command):
     ssh_command.run("opkg update")
-    ssh_command.run("opkg install tmate")
-    assert "tmate" in "\n".join(ssh_command.run("opkg list-installed")[0])
+    ssh_command.run("opkg install ucert")
+    assert "ucert" in "\n".join(ssh_command.run("opkg list-installed")[0])
