4fab43f3917642a1a0e67c55ee3ddd0580a6a30a
Chris Fiege <chris@tinyhost.de>
2025-06-21 22:33:46 +0200

    test:[apk|opkg] install ucert: Cleanup after test
    Test should not alter system state.
    So: Move the test code into a try..finally and try to uninstall `ucert`
    in the finally block.
    
    Also: Use `run_check` to run commands to fail on `rc != 0`.
    
    Signed-off-by: Chris Fiege <chris@tinyhost.de>

--- tests/test_apk.py
+++ tests/test_apk.py
@@ -8,5 +8,8 @@ def test_apk_procd_installed(shell_command):
 
 @pytest.mark.lg_feature(["online", "apk"])
 def test_apk_add_ucert(ssh_command):
-    ssh_command.run("apk add ucert")
-    assert "ucert" in "\n".join(ssh_command.run("apk list | grep ucert")[0])
+    try:
+        ssh_command.run_check("apk add ucert")
+        assert "ucert" in "\n".join(ssh_command.run_check("apk list | grep ucert"))
+    finally:
+        ssh_command.run("apk del ucert")
--- tests/test_opkg.py
+++ tests/test_opkg.py
@@ -8,6 +8,9 @@ def test_opkg_procd_installed(ssh_command):
 
 @pytest.mark.lg_feature(["online", "opkg"])
 def test_opkg_install_ucert(ssh_command):
-    ssh_command.run("opkg update")
-    ssh_command.run("opkg install ucert")
-    assert "ucert" in "\n".join(ssh_command.run("opkg list-installed")[0])
+    try:
+        ssh_command.run_check("opkg update")
+        ssh_command.run_check("opkg install ucert")
+        assert "ucert" in "\n".join(ssh_command.run_check("opkg list-installed"))
+    finally:
+        ssh_command.run("opkg remove ucert")
